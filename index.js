cfbe6cdbe895971caf9806c51215497f6e0b7e5c
FiggersRM <MitchFiggers@gmail.com>
2023-05-06 15:02:56 -0400

    Finished the creator functions for the shapes as well as returning the svg string. Also completed the creating of the logo.svg file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const shapes = require('./lib/shapes');
+const Shapes = require('./lib/shapes');
 
 inquirer
 .prompt ([
@@ -17,15 +17,29 @@ inquirer
     },
     {
         type: 'input',
-        message: 'Which color would you like your text to be? Please use a color keyword or a hexadecimal value',
+        message: 'Which color would you like your text to be? Please use a color keyword or a hexadecimal value including the #.',
         name: 'textColor',
     },
     {
         type: 'input',
-        message: 'Which color would you like you shape to be? Please use a color keyword or a hexadecimal number',
+        message: 'Which color would you like you shape to be? Please use a color keyword or a hexadecimal number including the #.',
         name: 'shapeColor'
     }
 ])
 .then ((response) => {
     data = response;
+    console.log(data);
+    if (data.shape === "Triangle") {
+        const triangle = new Shapes.Triangle(data.letters, data.textColor, data.shapeColor);
+        const svgStr = triangle.render();
+        fs.writeFile('logo.svg', svgStr, (err) => err ? console.error(err) : console.log('Generated logo.svg'));
+    } else if (data.shape === "Circle") {
+        const circle = new Shapes.Circle(data.letters, data.textColor, data.shapeColor);
+        const svgStr = circle.render();
+        fs.writeFile('logo.svg', svgStr, (err) => err ? console.error(err) : console.log('Generated logo.svg'));
+    } else {
+        const square = new Shapes.Square(data.letters, data.textColor, data.shapeColor);
+        const svgStr = square.render();
+        fs.writeFile('logo.svg', svgStr, (err) => err ? console.error(err) : console.log('Generated logo.svg'));
+    }
 })
